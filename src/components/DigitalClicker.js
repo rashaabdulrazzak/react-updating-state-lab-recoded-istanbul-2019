83113d82c9c86950d8cda8b4d509b4e0044609c4
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-10 16:57:20 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+import YouTubeDebugger from './YouTubeDebugger.js';
+
+class DigitalClicker extends React.Component{
+   constructor(){
+       super()
+       this.state = {
+        timesClicked:0
+       }
+   }
+   handleClick =()=>{
+    this.setState(previousState => {
+        return {
+        timesClicked: previousState.timesClicked + 1
+        }
+      })
+   }
+     render(){
+         return(
+             <div> 
+                 <button onClick={this.handleClick}> times has been clicked {this.state.timesClicked}</button>
+             </div>
+         )
+     }
+}
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,42 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react '
+class YouTubeDebugger extends Comment{
+    constructor(){
+        super()
+        this.state={
+                errors: [],
+                user: null,
+                settings: {
+                  bitrate: 8,
+                  video: {
+                    resolution: '1080p'
+                  }
+                }
+              }
+        
+    }
+    handleClick =()=>{
+        this.setState(previousState => {
+            return {
+              settings: {...previousState.settings,bitrate:12}
+            }
+          })
+    }
+    handleClickRes =()=>{
+        this.setState(previousState => {
+            return {
+              settings: {...previousState.settings, video:{
+                resolution: "720p"}}
+            }
+          })
+    }
+    render(){
+        return(
+            <div> 
+                <button className="bitrate" onClick={this.handleClick}> bitrate </button>
+                <button className="resolution" onClick={this.handleClickRes}> bitrate </button>
+            </div>
+        )
+    }
+}
+export default YouTubeDebugger
